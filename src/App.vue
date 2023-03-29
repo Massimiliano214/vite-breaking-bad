a89824a7086fc266397c6a86b309479ea4c7aef2
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-29 15:26:47 +0200

    creating header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
+    import TheHeader from "./components/TheHeader.vue";
 
+    export default {
+        components: {
+            TheHeader,
+        }
+    }
 </script>
 
 
 <template>
-
+<TheHeader></TheHeader>
 
 </template>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+
+<template>
+
+<h1 class="text-center">Yh-Gi-Oh Api</h1>
+    
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
