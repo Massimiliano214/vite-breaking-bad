ab7d6606fa669a3201d18a56c3fdcf55630be02c
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-30 16:11:57 +0200

    reworked if

--- src/components/CardSection.vue
+++ src/components/CardSection.vue
@@ -20,9 +20,11 @@
             listaFiltro(archetype) {
                 console.log(this.store.cardList.length );
             for(let i = 0; this.store.cardList.length > i; i++) {
-                if(!archetypeList[i].includes(this.store.cardList.archetype))
-                this.archetypeList.push(this.store.cardList.archetype);
-                console.log(this.store.cardList.archetype);
+                if(!archetypeList.includes(this.store.cardList.archetype)) {
+                    this.archetypeList.push(this.store.cardList.archetype);
+                    console.log(this.store.cardList.archetype);
+                }
+                    
             }
             }
             
